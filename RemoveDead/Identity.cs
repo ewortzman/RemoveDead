6157ed2da9815f9d47f48999b6624c131ae07679
Ethan Wortzman <ethan.wortzman@gmail.com>
2016-01-18 15:30:45 -0500

    reorganized, fixed initial implementation

---
+++ RemoveDead/Identity.cs
@@ -0,0 +1,18 @@
+﻿using ICities;
+
+namespace RemoveDead
+{
+    public class Identity : IUserMod
+    {
+        public string Name
+        {
+            get { return "RemoveDead"; }
+        }
+
+        public string Description
+        {
+            get { return "Removes all dead from all buildings, eliminating the need for deathcare."; }
+        }
+
+    }
+}
---
+++ RemoveDead/RemoveDead.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using ColossalFramework;
+using ColossalFramework.Plugins;
+
+using ICities;
+using UnityEngine;
+
+namespace RemoveDead
+{
+    public class RemoveDead : ThreadingExtensionBase
+    {
+        private CitizenManager _citizenManager;
+        private BuildingManager _buildingManager;
+
+        public override void OnCreated(IThreading threading)
+        {
+            _citizenManager = Singleton<CitizenManager>.instance;
+            _buildingManager = Singleton<BuildingManager>.instance;
+
+            base.OnCreated(threading);
+        }
+
+        public override void OnUpdate(float realTimeDelta, float simulationTimeDelta)
+        {
+            Citizen[] citizens = _citizenManager.m_citizens.m_buffer;
+            //FastList<ushort> healthBuildings = _buildingManager.GetServiceBuildings(ItemClass.Service.HealthCare);
+
+            for (uint i = 0; i < citizens.Length; i++)
+            {
+                Citizen citizen = citizens[i];
+
+                if (citizen.Dead)
+                {
+                    _citizenManager.ReleaseCitizen(i);
+                }
+            }
+
+            base.OnUpdate(realTimeDelta, simulationTimeDelta);
+        }
+
+        public override void OnReleased()
+        {
+            base.OnReleased();
+        }
+    }
+}
