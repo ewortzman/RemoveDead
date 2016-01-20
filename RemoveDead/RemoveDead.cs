70328aa3e2b74fe86745339f133a305bbeb1f2e0
Ethan Wortzman <ethan.wortzman@gmail.com>
2016-01-19 22:31:39 -0500

    first working version.  added gitignore. removed compiled files

--- RemoveDead/RemoveDead.cs
+++ RemoveDead/RemoveDead.cs
@@ -12,26 +12,49 @@ namespace RemoveDead
         private CitizenManager _citizenManager;
         private BuildingManager _buildingManager;
 
+        private float _delta = 0;
+
         public override void OnCreated(IThreading threading)
         {
             _citizenManager = Singleton<CitizenManager>.instance;
             _buildingManager = Singleton<BuildingManager>.instance;
-
+            
             base.OnCreated(threading);
         }
 
         public override void OnUpdate(float realTimeDelta, float simulationTimeDelta)
         {
-            Citizen[] citizens = _citizenManager.m_citizens.m_buffer;
-            //FastList<ushort> healthBuildings = _buildingManager.GetServiceBuildings(ItemClass.Service.HealthCare);
-
-            for (uint i = 0; i < citizens.Length; i++)
+            _delta += simulationTimeDelta;
+            
+            if (_delta > 300) // run remover every 5 in-game minutes
             {
-                Citizen citizen = citizens[i];
+                _delta = 0;
+                DebugOutputPanel.AddMessage(PluginManager.MessageType.Message, "iteration run");
 
-                if (citizen.Dead)
+                Citizen[] citizens = _citizenManager.m_citizens.m_buffer;
+                
+                int deadCount = 0;
+                int citizenCount = 0;
+
+                for (uint i = 0; i < citizens.Length; i++)
                 {
-                    _citizenManager.ReleaseCitizen(i);
+                    Citizen citizen = citizens[i];
+
+                    if (citizen.m_flags != Citizen.Flags.None) // Check if citizen struct has any flags (i.e. is the citizen initialized)
+                    {
+                        citizenCount++;
+                        
+                        if (citizen.Dead)
+                        {
+                            _citizenManager.ReleaseCitizen(i); // remove the citizen
+                            deadCount++;
+                        }
+                    }
+                }
+
+                if (deadCount > 0)
+                {
+                    DebugOutputPanel.AddMessage(PluginManager.MessageType.Message, "Removed " + deadCount + " dead bodies out of " + citizenCount + " citizens");
                 }
             }
 
